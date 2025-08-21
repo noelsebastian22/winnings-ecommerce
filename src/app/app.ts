9011b61fc0ff9e55129314ee3ae965a6c6eb488c
Noel Sebastian <noelsimc69@gmail.com>
2025-08-21 10:32:09 +1000

    feat: add header component with navigation and styling

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SpinnerComponent } from './shared/components/spinner/spinner.component';
+import { HeaderComponent } from './shared/components/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SpinnerComponent],
+  imports: [RouterOutlet, SpinnerComponent, HeaderComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
